d103ad7141264d87fbabe3e8f9ebf96797cdf7ec
accn <neudeck@amiconsult.de>
2025-03-07 17:37:51 +0100

    feat: create a catalog component, add loop

---
+++ assets/js/components/sidebar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div :class="[$style.sidebar,'p-3','mb-5']">
+        <h5 class="text-center">
+            Categories
+        </h5>
+        <ul class="nav flex-column mb4">
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/"
+                >All Products</a>
+            </li>
+
+            <li
+                v-for="(category, index) in categories"
+                :key="index"
+                class="nav-item"
+            >
+                <a
+                    class="nav-link"
+                    :href="category.link"
+                >
+                  {{ category.name }}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+  data() {
+      return {
+        categories: [
+          {
+            name: 'Dot Matrix Printers',
+            link: '#',
+          },
+          {
+            name: 'Flopy data',
+            link: '#',
+          }
+        ],
+      }
+  }
+};
+
+</script>
+
+<style lang="scss" module>
+@import '~styles/components/light-component';
+
+.sidebar {
+  @include light-component;
+
+  ul {
+    li a:hover {
+      background: $blue-component-link-hover;
+    }
+  }
+}
+</style>
